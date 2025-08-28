76e6df1de2b4f9de558166595ade205d90795bd4
Sarah Cole <sarahcole@Sarahs-MacBook-Air-2.local>
2025-08-27 23:39:09 -0700

    Add name JavaDoc

--- HelloWorld/src/org/howard/edu/lsp/assignment1/App.java
+++ HelloWorld/src/org/howard/edu/lsp/assignment1/App.java
@@ -1,5 +1,8 @@
 package org.howard.edu.lsp.assignment1;
 
+/**
+ * Name: Sarah Cole
+ */
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!!");
