1576ed37b003557cae9690e12839a9e67eeb2c9f
Sarah Cole <sarahcole@Sarahs-MacBook-Air-2.local>
2025-10-20 19:39:39 -0400

    added conceptual understanding

--- src/org/howard/edu/lsp/midterm/question2/Main.java
+++ src/org/howard/edu/lsp/midterm/question2/Main.java
@@ -3,11 +3,17 @@ package org.howard.edu.lsp.midterm.question2;
 public class Main {
         public static void main(String[] args) {
 
+        // Method overloading is the better approach
+        // It provides a cleaner intergave while unifying area calculations
+        // under a single class with multiple methods named 'area'.
+        // This enhances code readability and maintainability.
+
         System.out.println("Circle radius 3.0 → area = " + AreaCalculator.area(3.0));
         System.out.println("Rectangle 5.0 x 2.0 → area = " + AreaCalculator.area(5.0, 2.0));
         System.out.println("Triangle base 10, height 6 → area = " + AreaCalculator.area(10, 6));
         System.out.println("Square side 4 → area = " + AreaCalculator.area(4));
-
+        
+        // exeption handling 
         try {
             System.out.println("Attempting to calculate area with an invalid dimension...");
             AreaCalculator.area(-5);
